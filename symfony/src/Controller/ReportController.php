6ace2ff83d32d6491979e1f01d036dc7425d881c
MaximeMalecot <92maximemalecot@gmail.com>
2022-03-11 10:42:40 +0100

    charts splitted in sessions

--- symfony/src/Controller/ReportController.php
+++ symfony/src/Controller/ReportController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 
 use App\Entity\Brand;
 use App\Entity\Product;
+use App\Entity\Test;
 use App\Repository\ProductRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -44,22 +46,27 @@ class ReportController extends AbstractController
     }
 
     #[Route('/report/product/{id}', name: 'report_product')]
-    public function showProductReport(Product $product, ManagerRegistry $doctrine): Response
+    public function showProductReport(Request $request, Product $product, ManagerRegistry $doctrine): Response
     {
-        //$em = $doctrine->getManager();
-        $tests = $product->getTests()->getValues();
-        $records = null;
-        foreach ($tests as $test) {
-            $records[$test->getNbSession()][] = $test->getRecords()->getValues();
+        $em = $doctrine->getManager();
+        if(!$request->request->get('session')){
+            $session = 1;
+        }else{
+            $session = $request->query->get('session');
         }
-        dd($records);
-        return $this->render('report/products.html.twig', [ 
-            'products' => $pr->findBy([
-                'brand' => $brand
-            ])
+        $tests = $em->getRepository(Test::class)->findBy([
+            'product' => $product    
+        ]);
+        $availableSessions = [];
+        foreach($tests as $test){
+            if($test->getNbSession() == $session){
+                $records = $test->getRecords()->getValues();
+            }
+            $availableSessions[] = $test->getNbSession();
+        }
+        return $this->render('report/charts.html.twig', [
+            'sessions'=> $availableSessions,
+            'records' => $records
         ]);
     }
-
-
-
 }
